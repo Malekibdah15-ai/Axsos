3bd975f4ba0c6a024a4535a75211f7dabc9953bf
Malekibdah15-ai <mibdah15@gmail,com>
2025-09-16 16:10:42 +0300

    new

---
+++ Web-Fun/HTML/Dojo whether/dojo/script.js
@@ -0,0 +1,17 @@
+function convert(){
+    const tempSelect = document.getElementById("show-temp");
+    const unit = tempSelect.value;
+    
+
+}
+
+
+function display(el){
+    alert ("loading weather for "+ el.innerText);
+
+}
+
+function hide() {
+    var element = document.querySelector(".box");
+    element.remove();
+}
