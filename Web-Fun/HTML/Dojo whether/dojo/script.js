fa0bc2afac9c777543930193c6e27dac1dacf7d1
Malekibdah15-ai <mibdah15@gmail,com>
2025-09-16 19:15:39 +0300

    new

--- Web-Fun/HTML/Dojo whether/dojo/script.js
+++ Web-Fun/HTML/Dojo whether/dojo/script.js
@@ -1,10 +1,18 @@
-function convert(){
-    const tempSelect = document.getElementById("show-temp");
-    const unit = tempSelect.value;
-    
 
+function convert() {
+    const select = document.getElementById("show-temp");
+    const toFahrenheit = select.value === "f";
+    const temps = document.querySelectorAll(".temperature");
+
+    temps.forEach(temp => {
+        let value = parseInt(temp.dataset.value); 
+        if (toFahrenheit) {
+            temp.innerText = Math.round((value * 9 / 5) + 32) + "°F";
+        } else {
+            temp.innerText = value + "°C";
+        }
+    });
 }
-
 
 function display(el){
     alert ("loading weather for "+ el.innerText);
