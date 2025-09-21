55dbc8bbe9de59162459bcacdbddb2a7133c6f7b
Malekibdah15-ai <mibdah15@gmail,com>
2025-09-21 16:04:50 +0300

    Added python stack folder

---
+++ Python-Stack/Assignments/Hello world/Hello.py
@@ -0,0 +1,14 @@
+print ("Hello world") 
+
+name = "noelle"
+print ("Hello",name)
+print ("Hello"+name)
+
+name = 42
+print ("Hello",int(name))
+print ("Hello"+str(name))
+ 
+fav_food1 = "sushi" 
+fav_food2= "pizza"
+print (f"i love to eat {fav_food1} {fav_food2}")
+print ("i love to eat {} {}" .format(fav_food1, fav_food2))
