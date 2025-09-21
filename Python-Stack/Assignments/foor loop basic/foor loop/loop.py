243a12ae9e8a26478ee2d8f277ba014954e2d488
Malekibdah15-ai <mibdah15@gmail,com>
2025-09-21 16:19:10 +0300

    Added python stack folder

---
+++ Python-Stack/Assignments/foor loop basic/foor loop/loop.py
@@ -0,0 +1,35 @@
+
+for i in range(151):
+    print (i)
+
+for i in range (5, 1001, 5):
+    print (i)
+
+for i in range (1, 101, 1):
+
+
+    if i % 5 == 0:
+
+      print("coding")
+
+    elif i % 10 == 0:
+
+      print("codingDojo")
+
+    else:
+       print(i)
+sum = 0
+for i in range (0, 500001):
+    if i % 2 != 0:
+        sum+=i
+print(sum)
+
+for i in range (2018,0, -4):
+    print(i)
+
+lownum = 2
+mult = 3
+highnum = 9
+for i in range (3, 10, 3):
+    print(i)
+
