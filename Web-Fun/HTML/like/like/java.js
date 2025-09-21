d7699971f5e58057ad4e683ee815a41c070373c0
Malekibdah15-ai <mibdah15@gmail,com>
2025-09-21 15:43:31 +0300

    python stack and some web

--- Web-Fun/HTML/like/like/java.js
+++ Web-Fun/HTML/like/like/java.js
@@ -1,11 +1,30 @@
-class Solution {
-public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        for(let i = 0; i < nums.length; i++){
-            for(let j = 0; j < nums.length; j++){
-                if(nums[i] + nums[j] == target);
-                return [i,j];
-            }
-        }
-    }
-};
+for y in range (0, 150 1)
+priny y in range 
+
+for i in range(0, 1000, 5)
+print i
+
+
+for i in range(0, 101)
+
+if i % 10 == 0
+print (coding
+
+
+
+for i in range (0, 500001)
+if i == i+1
+print i
+
+for i in range (2018, 0, -4)
+print i
+
+
+lownum = 2
+mult = 3
+highnum = 9
+for lownum in range (2, 9, 3)
+print lownum
+
+
+
